d8ba8bc39be66da3e66d10a62e2d43133933f981
holden <holden@razrfly.com>
2025-07-26 13:04:15 +0200

    fixes

--- lib/cinegraph/movies/genre.ex
+++ lib/cinegraph/movies/genre.ex
@@ -2,10 +2,11 @@ defmodule Cinegraph.Movies.Genre do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @primary_key {:id, :id, autogenerate: true}
   schema "genres" do
     field :tmdb_id, :integer
     field :name, :string
+    
+    many_to_many :movies, Cinegraph.Movies.Movie, join_through: "movie_genres"
     
     timestamps()
   end
@@ -16,18 +17,16 @@ defmodule Cinegraph.Movies.Genre do
     |> cast(attrs, [:tmdb_id, :name])
     |> validate_required([:tmdb_id, :name])
     |> unique_constraint(:tmdb_id)
-    |> unique_constraint(:name)
   end
-
+  
   @doc """
-  Creates a changeset from TMDB API response data
+  Creates a changeset from TMDB data
   """
-  def from_tmdb(attrs) do
-    genre_attrs = %{
-      tmdb_id: attrs["id"],
-      name: attrs["name"]
+  def from_tmdb(tmdb_data) do
+    attrs = %{
+      tmdb_id: tmdb_data["id"],
+      name: tmdb_data["name"]
     }
-    
-    changeset(%__MODULE__{}, genre_attrs)
+    changeset(%__MODULE__{}, attrs)
   end
 end
