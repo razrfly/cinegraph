aea5de69582bd1de866ca02ca4e8128714b07763
holden <holden@razrfly.com>
2025-12-13 10:22:56 +0100

    finish cache

--- lib/cinegraph_web/plugs/cache_control_plug.ex
+++ lib/cinegraph_web/plugs/cache_control_plug.ex
@@ -30,9 +30,9 @@ defmodule CinegraphWeb.Plugs.CacheControlPlug do
       # HTML pages (LiveView) - prevent CDN caching entirely
       String.contains?(content_type, "text/html") ->
         conn
-        |> put_resp_header("cache-control", "no-store, no-cache, must-revalidate, max-age=0")
+        |> put_resp_header("cache-control", "private, no-store, no-cache, must-revalidate, max-age=0")
         |> put_resp_header("pragma", "no-cache")
-        |> put_resp_header("expires", "0")
+        |> put_resp_header("expires", "-1")
 
       # Already has cache-control set (e.g., redirects, static files)
       get_resp_header(conn, "cache-control") != [] ->
